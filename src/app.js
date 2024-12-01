619cb7c7cec28e5c04f54b22da4a9411e4eeaf29
preeti rana <preeti@titaniam.io>
2024-12-01 20:22:13 +0530

    test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res) => {
-  res.send("Hello from server");
+  res.send("Hello from server.");
 });
 
 app.listen(5000, () => {
